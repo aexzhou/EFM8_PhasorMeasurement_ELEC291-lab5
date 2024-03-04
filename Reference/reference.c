1138cb1ea0d17c34852e6d08a94b6e7fe66363a2
Alex Zhou <cosr330@gmail.com>
2024-03-03 18:35:09 -0800

    lab 5 first push

---
+++ Reference/reference.c
@@ -0,0 +1,15 @@
+unsigned int ADC_at_Pin(unsigned char pin)
+{
+	ADC0MX = pin;   // Select input from pin
+	ADINT = 0;
+	ADBUSY = 1;     // Convert voltage at the pin
+	while (!ADINT); // Wait for conversion to complete
+	return (ADC0);
+}
+
+float Volts_at_Pin(unsigned char pin)
+{
+	 return ((ADC_at_Pin(pin)*VDD)/0b_0011_1111_1111_1111);
+}
+
+
---
+++ note.md
@@ -0,0 +1,7 @@
+### Requirements & Notes
+---
+- display voltage on putty
+- use ADC on 14 bit mode
+    - check SFR PAGE
+    - ADBITS = 0x2
+- to get peak, either use quarter period and the voltage assiciated with that time, or just FindMax()
